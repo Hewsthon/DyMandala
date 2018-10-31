bd736a4d658cafa8f11a574284db5b37836b4457
Hewsthon <44635673+Hewsthon@users.noreply.github.com>
2018-10-31 12:27:08 -0400

    Update MyBot.js

--- MyBot.js
+++ MyBot.js
@@ -1,21 +1,12 @@
 const Discord = require("discord.js");
 const  client = new Discord.Client();
-const config = require("./config.json");
 
 client.on("ready", () => {
    console.log("Estoy listo!");
 });
-var prefix = config.prefix;
-
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix)) return;
-  if (message.author.bot) return;
-
-  if (message.content.startsWith(prefix + "tarao")) {
-    message.channel.send("Tarao e 🖕");
-  } else
-  if (message.content.startsWith(prefix +"hola")) {
-    message.channel.send("Hola que tal? 👁");
+  if (message.content.startsWith("Hola")) {
+    message.channel.send("¡Hola! soy DyMandala y fuí programado de 0 por mi dios Hewsthon para cuídar el servidor.");
   }
 });
 client.login("NTA2OTQ5MzIyNzg1Njg1NTIy.DrqH3g.YJVJvOjSHazXlP3_wCkp62cZTW0");
